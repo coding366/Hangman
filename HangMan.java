75fadc2981f68a193ec1e4617f5dcf4fb4b347f1
coding366 <69975419+coding366@users.noreply.github.com>
2020-08-20 22:30:46 +0500

    Update HangMan.java

--- HangMan.java
+++ HangMan.java
@@ -5,23 +5,26 @@ import java.util.Random;
 import java.util.Set;
 import java.util.Scanner;
 
-class Hangman {
+public class HangMan {
 
     public static void main(String[] args) {
-        int chances = 0;
-        Set<String> prevGuesses = new HashSet<>();
-        Scanner input = new Scanner(System.in);
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 5;//length of the word
         Random random = new Random();
- //generating random word using pre-built java8 method.
-    String wordToGuess = random.ints(leftLimit, rightLimit + 1)
-      .limit(targetStringLength)
-      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-      .toString();
-   
-       // System.out.println(wordToGuess);
+        //generating random word using pre-built java8 method.
+        String wordToGuess = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        System.out.println(wordToGuess);
+    }
+
+    void guessWord(String wordToGuess) {
+        int chances = 0;
+        Set<String> previousGuesses = new HashSet<>();
+        Scanner input = new Scanner(System.in);
 
         int length = wordToGuess.length();
         char[] wordToGuessChars = wordToGuess.toCharArray(); //creates character array of strings
@@ -40,26 +43,26 @@ class Hangman {
             System.out.println();
             System.out.println("Type your guess: ");
             String currentGuess = input.next().substring(0, 1);
-            char currentGuessChar = currentGuess.charAt(0); //gets char data from scanner
-            if (prevGuesses.contains(currentGuess)) //checks if user already guessed the letter previously
+            char currentGuessCharacter = currentGuess.charAt(0); //gets char data from scanner
+            if (previousGuesses.contains(currentGuess)) //checks if user already guessed the letter previously
             {
                 System.out.println("You already guessed this letter! Try again. Your previous guesses were: ");
-                System.out.println(prevGuesses.stream().reduce("", String::concat));
+                System.out.println(previousGuesses.stream().reduce("", String::concat));
                 repeated = true;
             }
-            prevGuesses.add(currentGuess);
+            previousGuesses.add(currentGuess);
             //if the guess is not a duplicated guess, it checks if the guessed letter is in the word
             if (!repeated) {
                 int times = 0; //number of times a letter is in the word
                 for (int index = 0; index < length; index++) {
-                    if (wordToGuessChars[index] == currentGuessChar) {
-                        censor[index] = currentGuessChar;  //replaces _ with guessed letter in caps
+                    if (wordToGuessChars[index] == currentGuessCharacter) {
+                        censor[index] = currentGuessCharacter;  //replaces _ with guessed letter in caps
                         correct = true;
                         times++;
                     }
                 }
                 if (correct) {
-                    System.out.println("The letter " + currentGuessChar + " is in the secret word! There are " + times + " " + currentGuessChar + " 's in the word. Revealing the letter(s): ");
+                    System.out.println("The letter " + currentGuessCharacter + " is in the secret word! There are " + times + " " + currentGuessCharacter + " 's in the word. Revealing the letter(s): ");
                 } else {
                     System.out.println("Sorry, the letter is not in the word. Your secret word:  ");
                 }
