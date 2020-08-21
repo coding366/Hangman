ca9172211454fee1bec133babf19d477f675b25b
coding366 <69975419+coding366@users.noreply.github.com>
2020-08-21 20:33:51 +0500

    Update UnitTest.java

--- UnitTest.java
+++ UnitTest.java
@@ -1,46 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testPackage;
-
-import hangman.HangMan;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class UnitTest {
-    
-    public UnitTest() {
+public class HangManClassTest {
+
+    public HangManClassTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
+    /**
+     * Test of main method, of class HangManClass.
+     */
     @Test
     public void testMain() {
+        System.out.println("main");
         String[] args = null;
-        HangMan.main(args);
-
+        HangManClass.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
-    
+
+    /**
+     * Test of toString method, of class HangManClass.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        HangManClass instance = new HangManClass();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+    @Test
+    public void testRandomGenerator() {
+        System.out.println("Random Generator");
+        HangManClass instance = new HangManClass();
+        instance.guessWord(instance.generateRandomWord());
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 }
